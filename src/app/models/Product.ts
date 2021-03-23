3ea41890d4bdbbb92533e7db20917d9545d5996b
Jakub Jereczek <99jakubjereczek@gmail.com>
2021-03-23 20:06:34 +0100

    service http and data model

---
+++ src/app/models/Product.ts
@@ -0,0 +1,5 @@
+export default interface Product {
+    Id: number,
+    InStock: boolean,
+    Name: string
+}
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Product } from '../models';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  URL_ADDRESS = 'https://localhost:44309/api/products/';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  // GET
+  getProducts(): Observable<Array<Product>> {
+    return this.http.get<Array<Product>>(this.URL_ADDRESS);
+
+  }
+
+  // GET/id
+  getProduct(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.URL_ADDRESS}/+${id}`);
+  }
+
+  // POST
+  addProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.URL_ADDRESS, product);
+  }
+
+  // PUT
+  updateProduct(product: Product): Observable<Product> {
+    return this.http.put<Product>(this.URL_ADDRESS, product)
+  }
+
+  // DELETE
+  deleteProduct(product: Product): Observable<Product> {
+    return this.http.delete<Product>(`${this.URL_ADDRESS}/+${product.Id}`);
+  }
+
+
+}
